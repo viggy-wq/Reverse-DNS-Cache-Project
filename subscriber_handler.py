fa13913e0aef68719c9c3c86c5a89c29f9b4f083
vignesh <vignesh@CHANDRASEKARANs-MacBook-Pro.local>
2024-02-25 18:00:48 -0800

    SubscriberDBSync now creates connection to db and feeds in requests for hostname_resolver

--- subscriber_handler.py
+++ subscriber_handler.py
@@ -6,57 +6,52 @@ import json
 import yaml
 import select
 import paho.mqtt.client as paho
+import time
 
 CREATE_IP_ADDRESS_LIST = '''CREATE TABLE IF NOT EXISTS ip_addresses
                  (ip_address TEXT PRIMARY KEY)'''
 
 INSERT_IP_ADDRESS = "INSERT OR IGNORE INTO ip_addresses (ip_address) VALUES (?)"
 
+config = {
+    'database': {'path': 'subscriber_db.sqlite'}
+}
+
 class SubscriberDBSync:
     def __init__(self, config):
         self.db_path = config['database']['path']
-        self.queue_size = config['queue']['size']
-        self.conn = sqlite3.connect(self.db_path)
+        self.client = paho.Client(client_id="test", callback_api_version=paho.CallbackAPIVersion.VERSION2)
+        self.client.on_message = self.onMessage
+        self.conn = sqlite3.connect(self.db_path, check_same_thread=False)
         cursor = self.conn.cursor()
         cursor.execute(CREATE_IP_ADDRESS_LIST)
         self.conn.commit()
+
+        if self.client.connect("localhost", 1883, 60) != 0:
+            print("Failed to connect to broker")
+            sys.exit(-1)
+
+        self.client.subscribe("ip_address")
+        self.client.loop_start()
     
     def insert(self, ip_address):
-        cursor = self.conn.cursor
+        cursor = self.conn.cursor()
         cursor.execute(INSERT_IP_ADDRESS,(ip_address,))
-        cursor.commit()
-        
-    def popQueue(self):
-        cursor = self.conn.cursor
-        cursor.execute("SELECT ip_address FROM ip_addresses LIMIT ?", (self.queue_size,))
-        addresses = cursor.fetchall()
-        print(addresses)
+        self.conn.commit()
+            
+    def onMessage(self, client, userdata, msg):
+        if msg.topic == "ip_address":
+            ip_address = msg.payload.decode()
+            self.insert(ip_address)
+            print(f"Inserted IP address: {ip_address}")
 
-        # TODO read in self.queue_size number of ip_addresses from the sqlite db into a queue while deleting the entries placed in the queue from the db and return the queue
-    
+    def print_db(self):
+        cursor = self.conn.cursor()
+        cursor.execute('SELECT * FROM ip_addresses')
+        for row in cursor.fetchall():
+            print(row)
 
-def onMessage(client, userdata, msg):
-    if msg.topic == "ip_address":
-        # TODO db.insert(address) possibly then sleep to prevent onMessage being stuck on ip_address topic
-
-    if msg.topic == "availability":
-        # TODO if DNSCacheSync ready for queue by posting ready in avaiability topic popQueue and publish queue contents to "ip_address_queue" topic
-
-client = paho.Client(client_id="test", callback_api_version=paho.CallbackAPIVersion.VERSION2)
-client.on_message = onMessage
-
-if client.connect("localhost", 1883, 60) != 0:
-    print("Failed to connect to broker")
-    sys.exit(-1)
-
-client.subscribe("test")
-
-try:
-    client.loop_forever()
-except:
-    print("Disconnecting")
-
-
-client.disconnect()
-
+subscriber_db_sync = SubscriberDBSync(config)
+time.sleep(10)
+subscriber_db_sync.print_db()
 
